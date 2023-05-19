c64bb710fcbb22e4aa811ba6cce083e8653d6c11
Vitoz <vitozb@gmail.com>
2023-05-19 16:51:57 +0600

    Add If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input user name ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "vitoz")
+{
+    Console.WriteLine("Hello master");
+}
+else
+{
+    Console.WriteLine("Hello ");
+    Console.WriteLine(username);
+}
